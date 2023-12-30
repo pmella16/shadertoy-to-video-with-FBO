1d8833e69edf56cb78ed93435b539c80e1751e81
Danil <s48gs.w@gmail.com>
2023-12-30 23:28:24 +0300

    Update render.sh

--- example_cubemap_recording/render.sh
+++ example_cubemap_recording/render.sh
@@ -3,6 +3,7 @@
 # remember to add shift_per_side to size if used (here not used)
 # to control "side per buffer" - used start_mouse_ in Buf0-5
 # if you need to rotate camera - do it in main_image.glsl
+# remember alo you need 90 deg camera FOV to record sides
 # use https://github.com/google/spatial-media/releases/tag/v2.0 to convert to 360 video on youtube
 
 python3 ../shadertoy-render.py --output 3.mp4 --size=512x512 --panorama_rec_size=1280x720 --rate=30 --duration=3. --bitrate=5M  main_image.glsl
